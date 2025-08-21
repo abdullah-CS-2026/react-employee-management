ffd347f28ca06bc7bf86c7af5c39305ba56f32cb
Abdullah-536 <iamabdullah536@gmail.com>
2025-08-20 22:27:16 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Form } from './Pages/Form'
 import { ShowDetails } from './Pages/ShowDetails';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router=createBrowserRouter([
-      {path:'/',element:<Form/>},
-      {path:'/employees',element:<ShowDetails/>},
-      {path:'/form/:id', element:<Form/>}
-    ])
+const router = createBrowserRouter([
+  { path: '/', element: <Form /> },
+  { path: '/employees', element: <ShowDetails /> },
+  { path: '/form/:id', element: <Form /> }
+])
 function App() {
   return (
-    
-    <RouterProvider router={router}/>
+
+    <RouterProvider router={router} />
 
   )
 }
